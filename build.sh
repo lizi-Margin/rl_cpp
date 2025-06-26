67cf22060b0debb5d672ccc3d3786ba9690fcff2
hulc <3314986382@qq.com>
2025-06-26 14:45:19 +0800

    use libtorch-cpu

--- build.sh
+++ build.sh
@@ -3,13 +3,12 @@
 mkdir -p build
 cd build || exit 1
 
-export CPATH="$CPATH"":/opt/libtorch/include/"
-export CPATH="$CPATH"":/opt/libtorch/include/torch/csrc/api/include/"
-
 cmake \
-  -DCMAKE_PREFIX_PATH=/opt/libtorch \
+  -DCMAKE_PREFIX_PATH='/opt/libtorch-cpu;/opt/cuda' \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
   .. && make -j12
 cd ..
 
-./build/rl
+if [ "$1" = "run" ]; then
+  ./build/rl
+fi
